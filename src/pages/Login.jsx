007999a1973cbe8ddc3aa05ef9f7debdcb58d106
khaleelulvarisep <khaleelulvaris@gmail.com>
2026-01-30 15:29:50 +0530

    api change

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -62,7 +62,7 @@ function Login() {
 
     setLoading(true);
     try {
-      const res = await axios.post("http://fitzbackend-env.eba-ygza6tbr.eu-north-1.elasticbeanstalk.com/api/auth/login/", {
+      const res = await axios.post("https://fitzbackend-env.eba-ygza6tbr.eu-north-1.elasticbeanstalk.com/api/auth/login/", {
         email,
         password,
       });
